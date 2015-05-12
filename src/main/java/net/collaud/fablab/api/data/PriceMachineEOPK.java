b682a57256d03ffd0e514d0c9acbe033051d961d
Gaetan Collaud <gaetancollaud@gmail.com>
2015-05-12 20:01:09 +0200

    sql fix

--- src/main/java/net/collaud/fablab/api/data/PriceMachineEOPK.java
+++ src/main/java/net/collaud/fablab/api/data/PriceMachineEOPK.java
@@ -1,13 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package net.collaud.fablab.api.data;
 
 import java.io.Serializable;
-import javax.persistence.Column;
-import javax.persistence.Embeddable;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
