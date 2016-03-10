f748804e22285ec7d8e79185cdaf1cc9df52d2a6
gaetancollaud <gaetancollaud@gmail.com>
2016-03-10 21:00:21 +0100

    masonry

--- src/main/webapp/components/project/project-list-ctrl.js
+++ src/main/webapp/components/project/project-list-ctrl.js
@@ -1,12 +1,19 @@
-angular.module('Fablab').controller('ProjectListController', function ($scope, $filter, $location, ProjectService) {
+angular.module('Fablab').controller('ProjectListController', function ($scope, $filter, $timeout, ProjectService) {
 
 	var updateProjectsList = function () {
 		ProjectService.list(function (data) {
 			$scope.projects = data;
+			$timeout(function () {
+				$('.grid').masonry({
+					columnWidth: 180,
+					isFitWidth: true
+				});
+			}, 100);
 		});
 	};
 
 	updateProjectsList();
+
 
 });
 
