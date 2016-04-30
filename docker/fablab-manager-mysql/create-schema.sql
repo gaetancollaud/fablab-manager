5e424f0378365bba0986be86d97bfc4152edb8d1
gaetan <gaetancollaud@gmail.com>
2016-04-30 23:01:41 +0200

    docker

---
+++ docker/fablab-manager-mysql/create-schema.sql
@@ -0,0 +1,6 @@
+-- -----------------------------------------------------
+-- Schema fablab
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `fablab` ;
+CREATE SCHEMA IF NOT EXISTS `fablab` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+USE `fablab` ;
--- docker/fablab-manager-springboot/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-java -jar fablab-manager*.jar \
-	--spring.datasource.url="jdbc:mysql://$MYSQL_PORT_3306_TCP_ADDR:$MYSQL_PORT_3306_TCP_PORT/$MYSQL_ENV_MYSQL_DATABASE"
-	--spring.datasource.username=$MYSQL_ENV_MYSQL_USER
-	--spring.datasource.password=$MYSQL_ENV_MYSQL_PASSWORD
