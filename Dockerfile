7a400cd9cdd992076c4312843162282016f9a9de
Gaetan Collaud <gaetancollaud@gmail.com>
2016-08-06 17:19:14 +0200

    docker improvements

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM java:8
 VOLUME /tmp
-#ADD entrypoint.sh .
-ADD fablab-manager*.jar ./
+ADD target/fablab-manager*.jar ./
 RUN mv fablab-manager*.jar app.jar
 RUN sh -c 'touch /app.jar'
 ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","app.jar"]
--- docker/fablab-manager-mysql/create-schema.sql
+++
@@ -1,6 +0,0 @@
--- -----------------------------------------------------
--- Schema fablab
--- -----------------------------------------------------
-DROP SCHEMA IF EXISTS `fablab` ;
-CREATE SCHEMA IF NOT EXISTS `fablab` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `fablab` ;
